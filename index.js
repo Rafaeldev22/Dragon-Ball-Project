8f0b264a942eaeb53fdbc00cd20977a9e74b4152
Rafaeldev22 <rafael.dev.22@gmail.com>
2023-05-25 22:28:55 -0300

    meu commit Dragon Ball Project

---
+++ index.js
@@ -0,0 +1,11 @@
+var sound1 = new Audio();
+sound1.src="audios/goku.mp3";
+
+var sound2 = new Audio();
+sound2.src="audios/gohan.mp3";
+
+var sound3 = new Audio();
+sound3.src="audios/thunks.mp3";
+
+var sound4 = new Audio();
+sound4.src="audios/vejeta.mp3";
